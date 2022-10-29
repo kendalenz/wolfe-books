51bc997637263d59f6b34ba3e84cd827902419a0
Kendal Enz <kendal.enz@gmail.com>
2022-10-29 15:35:13 -0400

    updated Product model

--- server/db/Product.js
+++ server/db/Product.js
@@ -1,5 +1,5 @@
 const conn = require('./conn');
-const { STRING, UUID, UUIDV4 } = conn.Sequelize;
+const { STRING, UUID, UUIDV4, DECIMAL } = conn.Sequelize;
 
 const Product = conn.define('product', {
   id: {
@@ -7,13 +7,34 @@ const Product = conn.define('product', {
     primaryKey: true,
     defaultValue: UUIDV4
   },
-  name: {
+  author: {
     type: STRING,
     allowNull: false,
     validate: {
       notEmpty: true
     }
   },
+  title: {
+    type: STRING,
+    allowNull: false,
+    Validate: {
+      notEmpty: true
+    },
+    description: {
+      type: TEXT,
+      allowNull: false,
+      Validate: {
+        notEmpty: true
+      }
+    },
+    price: {
+      type: DECIMAL,
+      allowNull: false,
+      Validate: {
+        notEmpty: true
+      }
+    }
+  }
 });
 
 module.exports = Product;
